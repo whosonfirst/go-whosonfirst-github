8b7642dbe781c7edd4e4f28d75be22520d93bdee
thisisaaronland <aaron@localhost>
2021-07-19 13:27:24 -0700

    update vendor deps

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/whosonfirst/go-whosonfirst-github
 go 1.12
 
 require (
-	github.com/briandowns/spinner v1.11.1
+	github.com/briandowns/spinner v1.16.0
 	github.com/google/go-github/v27 v27.0.6
-	github.com/sfomuseum/go-flags v0.4.2
+	github.com/sfomuseum/go-flags v0.8.2
 	github.com/whosonfirst/iso8601duration v0.0.0-20150204201828-8da3af7a2a61
-	golang.org/x/oauth2 v0.0.0-20200902213428-5d25da1a8d43
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
 )
--- vendor/github.com/briandowns/spinner/spinner.go
+++ vendor/github.com/briandowns/spinner/spinner.go
@@ -14,13 +14,13 @@
 package spinner
 
 import (
-	"encoding/hex"
 	"errors"
 	"fmt"
 	"io"
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 	"unicode/utf8"
@@ -96,6 +96,9 @@ var validColors = map[string]bool{
 	"bgHiWhite":   true,
 }
 
+// returns true if the OS is windows and the WT_SESSION env variable is set.
+var isWindowsTerminalOnWindows = len(os.Getenv("WT_SESSION")) > 0 && runtime.GOOS == "windows"
+
 // returns a valid color's foreground text color attribute
 var colorAttributeMap = map[string]color.Attribute{
 	// default colors for backwards compatibility
@@ -163,10 +166,7 @@ var colorAttributeMap = map[string]color.Attribute{
 
 // validColor will make sure the given color is actually allowed.
 func validColor(c string) bool {
-	if validColors[c] {
-		return true
-	}
-	return false
+	return validColors[c]
 }
 
 // Spinner struct to hold the provided options.
@@ -271,9 +271,9 @@ func (s *Spinner) Start() {
 		s.mu.Unlock()
 		return
 	}
-	if s.HideCursor && runtime.GOOS != "windows" {
+	if s.HideCursor && !isWindowsTerminalOnWindows {
 		// hides the cursor
-		fmt.Print("\033[?25l")
+		fmt.Fprint(s.Writer, "\033[?25l")
 	}
 	s.active = true
 	s.mu.Unlock()
@@ -285,11 +285,14 @@ func (s *Spinner) Start() {
 				case <-s.stopChan:
 					return
 				default:
+					s.mu.Lock()
 					if !s.active {
+						s.mu.Unlock()
 						return
 					}
-					s.mu.Lock()
-					s.erase()
+					if !isWindowsTerminalOnWindows {
+						s.erase()
+					}
 
 					if s.PreUpdate != nil {
 						s.PreUpdate(s)
@@ -303,9 +306,9 @@ func (s *Spinner) Start() {
 							outColor = fmt.Sprintf("\r%s%s%s ", s.Prefix, s.color(s.chars[i]), s.Suffix)
 						}
 					} else {
-						outColor = fmt.Sprintf("%s%s%s ", s.Prefix, s.color(s.chars[i]), s.Suffix)
+						outColor = fmt.Sprintf("\r%s%s%s ", s.Prefix, s.color(s.chars[i]), s.Suffix)
 					}
-					outPlain := fmt.Sprintf("%s%s%s ", s.Prefix, s.chars[i], s.Suffix)
+					outPlain := fmt.Sprintf("\r%s%s%s ", s.Prefix, s.chars[i], s.Suffix)
 					fmt.Fprint(s.Writer, outColor)
 					s.lastOutput = outPlain
 					delay := s.Delay
@@ -328,13 +331,17 @@ func (s *Spinner) Stop() {
 	defer s.mu.Unlock()
 	if s.active {
 		s.active = false
-		if s.HideCursor && runtime.GOOS != "windows" {
+		if s.HideCursor && !isWindowsTerminalOnWindows {
 			// makes the cursor visible
-			fmt.Print("\033[?25h")
+			fmt.Fprint(s.Writer, "\033[?25h")
 		}
 		s.erase()
 		if s.FinalMSG != "" {
-			fmt.Fprintf(s.Writer, s.FinalMSG)
+			if isWindowsTerminalOnWindows {
+				fmt.Fprint(s.Writer, "\r", s.FinalMSG)
+			} else {
+				fmt.Fprint(s.Writer, s.FinalMSG)
+			}
 		}
 		s.stopChan <- struct{}{}
 	}
@@ -349,13 +356,14 @@ func (s *Spinner) Restart() {
 // Reverse will reverse the order of the slice assigned to the indicator.
 func (s *Spinner) Reverse() {
 	s.mu.Lock()
-	defer s.mu.Unlock()
 	for i, j := 0, len(s.chars)-1; i < j; i, j = i+1, j-1 {
 		s.chars[i], s.chars[j] = s.chars[j], s.chars[i]
 	}
+	s.mu.Unlock()
 }
 
-// Color will set the struct field for the given color to be used.
+// Color will set the struct field for the given color to be used. The spinner
+// will need to be explicitly restarted.
 func (s *Spinner) Color(colors ...string) error {
 	colorAttributes := make([]color.Attribute, len(colors))
 
@@ -370,45 +378,34 @@ func (s *Spinner) Color(colors ...string) error {
 	s.mu.Lock()
 	s.color = color.New(colorAttributes...).SprintFunc()
 	s.mu.Unlock()
-	s.Restart()
 	return nil
 }
 
 // UpdateSpeed will set the indicator delay to the given value.
 func (s *Spinner) UpdateSpeed(d time.Duration) {
 	s.mu.Lock()
-	defer s.mu.Unlock()
 	s.Delay = d
+	s.mu.Unlock()
 }
 
 // UpdateCharSet will change the current character set to the given one.
 func (s *Spinner) UpdateCharSet(cs []string) {
 	s.mu.Lock()
-	defer s.mu.Unlock()
 	s.chars = cs
+	s.mu.Unlock()
 }
 
 // erase deletes written characters.
 // Caller must already hold s.lock.
 func (s *Spinner) erase() {
 	n := utf8.RuneCountInString(s.lastOutput)
-	if runtime.GOOS == "windows" {
-		clearString := "\r"
-		for i := 0; i < n; i++ {
-			clearString += " "
-		}
-		clearString += "\r"
-		fmt.Fprintf(s.Writer, clearString)
+	if runtime.GOOS == "windows" && !isWindowsTerminalOnWindows {
+		clearString := "\r" + strings.Repeat(" ", n) + "\r"
+		fmt.Fprint(s.Writer, clearString)
 		s.lastOutput = ""
 		return
 	}
-	del, _ := hex.DecodeString("7f")
-	for _, c := range []string{"\b", string(del)} {
-		for i := 0; i < n; i++ {
-			fmt.Fprintf(s.Writer, c)
-		}
-	}
-	fmt.Fprintf(s.Writer, "\r\033[K") // erases to end of line
+	fmt.Fprintf(s.Writer, "\033[K") // erases to end of line
 	s.lastOutput = ""
 }
 
